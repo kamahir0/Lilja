55c4b56a90fc48afd39eef9b04e0b2df8cbd8cc2
kamahir0 <hrtk1330@gmail.com>
2026-01-24 23:28:59 +0900

    テンプレート修正

---
+++ lilja-packages/lilja.dev-kit/Editor/Templates~/Package/Runtime/Example.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace #DISPLAY_NAME#
+{
+    public class Example : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from #DISPLAY_NAME# Runtime!");
+        }
+    }
+}
