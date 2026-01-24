581207af527295d449b053109b850c37370202a4
kamahir0 <hrtk1330@gmail.com>
2026-01-24 17:08:34 +0900

    ディレクトリ整理

---
+++ lilja-packages/lilja.roslyn-test/Analyzer~/Lilja.RoslynTest.Analyzer/HelloGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MyUnityGenerator
+{
+    [Generator]
+    public class HelloGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // ビルド時に必ず実行される登録
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                ctx.AddSource("Test.g.cs", "// Auto Generated Code \n public class GeneratedClass { }");
+            });
+        }
+    }
+}
