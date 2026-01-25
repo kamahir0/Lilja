d11b9c28ccd8576860d21f4ff592281c235fa79e
kamahir0 <hrtk1330@gmail.com>
2026-01-25 23:18:34 +0900

    テストコード

---
+++ sandbox/Sandbox-6000.3.2f1/Assets/AssetManagementTest/ResourcesLoaderTest.cs
@@ -0,0 +1,69 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Lilja.AssetManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AssetManagementTest
+{
+    public class ResourcesLoaderTest : MonoBehaviour
+    {
+        [SerializeField] private Image _image;
+        [SerializeField] private string _path = "lilja_1";
+
+        private AssetLifetime _lifetime;
+        private IAssetLoader _loader;
+
+        private void Start()
+        {
+            _loader = new ResourcesAssetLoader();
+
+            UniTask.Void(async () =>
+            {
+                await Load();
+                await UniTask.Delay(1000);
+                Unload();
+            });
+        }
+
+        public async UniTask Load()
+        {
+            if (_lifetime != null)
+            {
+                Debug.LogWarning("Already loaded. Dispose first.");
+                return;
+            }
+
+            _lifetime = new AssetLifetime();
+
+            Debug.Log($"Loading {_path}...");
+            try
+            {
+                var sprite = await _loader.LoadAsync<Sprite>(_path, _lifetime, this.GetCancellationTokenOnDestroy());
+                _image.sprite = sprite;
+                Debug.Log($"Loaded: {sprite.name}");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to load: {e}");
+                _lifetime.Dispose();
+                _lifetime = null;
+            }
+        }
+
+        public void Unload()
+        {
+            if (_lifetime == null)
+            {
+                Debug.LogWarning("Not loaded.");
+                return;
+            }
+
+            Debug.Log("Unloading...");
+            _lifetime.Dispose();
+            _lifetime = null;
+            Debug.Log("Unloaded.");
+        }
+    }
+}
+
