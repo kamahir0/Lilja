ed1f22bc69e8ee2f4cba555855790c02cdba8737
kamahir0 <hrtk1330@gmail.com>
2026-01-25 12:28:04 +0900

    AssetManagement作成

---
+++ lilja-packages/lilja.asset-management/src/Scripts/Example.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Lilja.AssetManagement
+{
+    public class Example : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from Lilja.AssetManagement Runtime!");
+        }
+    }
+}
