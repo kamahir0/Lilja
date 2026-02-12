45e4f97a76e96204420615a9af56085941c10f95
kamahir0 <hrtk1330@gmail.com>
2026-02-13 00:05:36 +0900

    MCP

---
+++ lilja-packages/lilja.mcp/src/Scripts/Example.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Lilja.MCP
+{
+    public class Example : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from Lilja.MCP Runtime!");
+        }
+    }
+}
